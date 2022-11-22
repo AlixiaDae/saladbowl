0ad88b507e61c5379d30725bf5fc0e629715b6d6
AlixiaDae <lizziejones1208@gmail.com>
2022-11-22 20:42:57 +0800

    Create navigation and footer bar

--- src/index.js
+++ src/index.js
@@ -1,5 +1,47 @@
 import homePage from "./homePage";
 
-const content = document.getElementById('content')
 
-content.appendChild(homePage())
+function navBar() {
+    const navWrapper = document.createElement('div')
+    navWrapper.classList.add('nav-bar')
+
+    const logo = document.createElement('div')
+    logo.textContent = 'Salad Bowl'
+    logo.classList.add('logo')
+    navWrapper.appendChild(logo)
+
+    const homeBtn = document.createElement('div')
+    homeBtn.textContent = 'Home'
+    navWrapper.appendChild(homeBtn)
+
+    const menuBtn = document.createElement('div')
+    menuBtn.textContent = 'Menu'
+    navWrapper.appendChild(menuBtn)
+
+    const contactBtn = document.createElement('div')
+    contactBtn.textContent = 'Contact'
+    navWrapper.appendChild(contactBtn)
+
+    return navWrapper
+}
+
+function footerBar() {
+    const footer = document.createElement('div')
+    footer.classList.add('footer')
+
+    const footerLeft = document.createElement('div')
+    footerLeft.textContent = '©Salad Bowl 2022'
+    footer.appendChild(footerLeft)
+
+    const footerRight = document.createElement('div')
+    footerRight.textContent = 'Made for OdinProject by AlixiaDae'
+    footer.appendChild(footerRight)
+
+    return footer
+}
+
+const content = document.getElementById('content')
+    content.appendChild(navBar())
+    content.appendChild(homePage())
+    content.appendChild(footerBar())
+
